b7e0c6b82c0cbd0f30c030f3d136ac57e018da66
Tracy Ngo <tngo1206@gmail.com>
2019-03-09 21:05:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each do |attribute, attribute_hash|
-    attribute_hash.each do |attribute_key, attribute_value|
+    attribute_hash.each do |attribute_key.to_s, attribute_value|
       attribute_value.each do |name|
         if new_hash.has_key?(name)
           if new_hash[name].has_key?(attribute)
